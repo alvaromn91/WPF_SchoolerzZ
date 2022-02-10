41fd8261c4dae7518b3e968027bfae8d3776599a
alvaromn91 <alvaromn91@gmail.com>
2022-02-10 15:53:36 +0100

    Arreglo OpenFileDialog

--- SchoolerzZ/SchoolerzZ/AccesoDatos.cs
+++ SchoolerzZ/SchoolerzZ/AccesoDatos.cs
@@ -1,50 +1,50 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Data;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using MySql.Data.MySqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
 
-//namespace CapaAccDatosB
-//{
-//    public class AccesoDatos
-//    {
-//        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=1234;database=scholerzz;";
-//        MySqlConnection databaseConnection;
+namespace CapaAccDatosB
+{
+    public class AccesoDatos
+    {
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=1234;database=scholerzz;";
+        MySqlConnection databaseConnection;
 
-//        public AccesoDatos()
-//        {
-//            databaseConnection = new MySqlConnection(connectionString);
-//        }
-//        public AccesoDatos(string servidorDatos,string puerto,string u, string pwd, string bbdd)
-//        {
-//            databaseConnection = new MySqlConnection("datasource=" + servidorDatos + ";port=" + puerto + ";username=" + u + ";password= " + pwd +";database=" + bbdd +";");
-//        }
-//        private void EstablecerConexion()
-//        {
-//            databaseConnection.Open();
-//        }
+        public AccesoDatos()
+        {
+            databaseConnection = new MySqlConnection(connectionString);
+        }
+        public AccesoDatos(string servidorDatos,string puerto,string u, string pwd, string bbdd)
+        {
+            databaseConnection = new MySqlConnection("datasource=" + servidorDatos + ";port=" + puerto + ";username=" + u + ";password= " + pwd +";database=" + bbdd +";");
+        }
+        private void EstablecerConexion()
+        {
+            databaseConnection.Open();
+        }
         
-//        public void CerrarConexion()
-//        {
-//            databaseConnection.Close();
-//        }
+        public void CerrarConexion()
+        {
+            databaseConnection.Close();
+        }
        
-//        public int Login(char pc_Type, string pv_Nick , string pv_Password )
-//        {
-//            MySqlCommand cmd = new MySqlCommand("SELECT Login(?, ?, ?)", databaseConnection);
-//            cmd.CommandType = CommandType.Text;
-//            cmd.Parameters.AddWithValue("pc_Type",pc_Type);
-//            cmd.Parameters.AddWithValue("pv_Nick", pv_Nick);
-//            cmd.Parameters.AddWithValue("pv_Password", pv_Password);
+        public int Login(char pc_Type, string pv_Nick , string pv_Password )
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT Login(?, ?, ?)", databaseConnection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("pc_Type",pc_Type);
+            cmd.Parameters.AddWithValue("pv_Nick", pv_Nick);
+            cmd.Parameters.AddWithValue("pv_Password", pv_Password);
 
-//            EstablecerConexion();
-//            int s = cmd.ExecuteNonQuery();
-//            CerrarConexion();
-//            return  s; //Lista<Object>
-//        }
-//    }
-//}
+            EstablecerConexion();
+            int s = cmd.ExecuteNonQuery();
+            CerrarConexion();
+            return  s; //Lista<Object>
+        }
+    }
+}
--- SchoolerzZ/SchoolerzZ/AppManager.cs
+++ SchoolerzZ/SchoolerzZ/AppManager.cs
@@ -1,34 +1,34 @@
-﻿//using System.Collections.Generic;
-//using System.Windows.Media;
+﻿using System.Collections.Generic;
+using System.Windows.Media;
 
-//namespace CapaAccDatosB
-//{
-//    internal class AppManager
-//    {
-//        private AccesoDatos accData = new AccesoDatos();
-//        private MediaPlayer mpMusic = new MediaPlayer();
-//        private MediaPlayer mpSFX = new MediaPlayer();
-
-
-//        public AppManager()
-//        {
+namespace CapaAccDatosB
+{
+    internal class AppManager
+    {
+        private AccesoDatos accData = new AccesoDatos();
+        private MediaPlayer mpMusic = new MediaPlayer();
+        private MediaPlayer mpSFX = new MediaPlayer();
 
 
-//        }
-
-//        public List<object> InsertarPelicula(List<object> pelicula)
-//        {
-//            return accData.EjecutarProcedimiento("AgregarPelicula", pelicula);
-//        }
-//        public void ActualizarActor(string nuevo_nombre,
-//                                    string nuevo_apellido,
-//                                    string old_nombre,
-//                                    string old_apellido)
-//        {
-
-//        }
+        public AppManager()
+        {
 
 
+        }
 
-//    }
-//}
+        public List<object> InsertarPelicula(List<object> pelicula)
+        {
+            return accData.EjecutarProcedimiento("AgregarPelicula", pelicula);
+        }
+        public void ActualizarActor(string nuevo_nombre,
+                                    string nuevo_apellido,
+                                    string old_nombre,
+                                    string old_apellido)
+        {
+
+        }
+
+
+
+    }
+}
