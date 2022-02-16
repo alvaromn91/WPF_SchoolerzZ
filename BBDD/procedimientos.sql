8fda7241103f069702027a82612c8e5156c0c19e
alvaromn91 <alvaromn91@gmail.com>
2022-02-16 18:07:50 +0100

    enesima modificacion de la DB

--- BBDD/procedimientos.sql
+++ BBDD/procedimientos.sql
@@ -61,59 +61,61 @@ create procedure AddStudent(in pv_School_Manager_Nick varchar(50),
                             in pt_Medical TEXT,
                             in pt_Observations TEXT,
                             in pt_Photo_Internal_Route TEXT,
-                            in pv_Licence varchar(29),
                             out pi_r int)
 begin 
 	declare vv_nick varchar(15);
     declare vv_school_name varchar(50);
     declare vb_id binary(16);
-    set vv_school_name = (select SZ_006_Name from sz_006_schools join sz_007_school_managers on SZ_006_Id = SZ_007_Schools_Id where SZ_007_Nick like upper(pv_School_Manager_Nick)); 
-   
-    INSERT INTO `schoolerzz`.`sz_002_students`
-    (`SZ_002_Id`,
-    `SZ_002_Name`,
-    `SZ_002_SN1`,
-    `SZ_002_SN2`,
-    `SZ_002_Birth`,
-    `SZ_002_Nationality`,
-    `SZ_002_Country`,
-    `SZ_002_City`,
-    `SZ_002_PostalCode`,
-    `SZ_002_Address`,
-    `SZ_002_Email`,
-    `SZ_002_Password`
-    )
-    VALUES
-    (UUID_TO_BIN(UUID()),
-    pv_Name,
-    pv_SN1,
-    pv_SN2,
-    pdt_Birth,
-    pv_Nationality,
-    pv_Country,
-    pv_City,
-    pv_PostalCode,
-    pv_Address,
-    pv_Email,
-    pv_Password
-    );
-    set vb_id = (SELECT SZ_002_Id from sz_002_students where SZ_002_Name like pv_Name and SZ_002_SN1 like pv_SN1 and SZ_002_SN2 like pv_SN2);
-    if pt_Medical not like '' then
-		update sz_002_students set SZ_002_Medical = pt_Medical where SZ_002_Id = vb_id;
-    end if;
-    if pv_SN2 not like '' then
-		update sz_002_students set SZ_002_SN2 = pv_SN2 where SZ_002_Id = vb_id;
-    end if;
-    if pt_Observations not like '' then
-		update sz_002_students set SZ_002_Observations = pt_Observations where SZ_002_Id = vb_id;
-    end if;
-    if pt_Photo_Internal_Route not like '' then
-		update sz_002_students set SZ_002_Photo_Internal_Route = pt_Photo_Internal_Route where SZ_002_Id = vb_id;
-    end if;
-    call CreateNick('S', vv_school_name, vb_id, @r_nick);
-    UPDATE sz_002_students SET SZ_002_Nick = (select @r_nick) WHERE SZ_002_Id = vb_id;
-    UPDATE sz_015_Licences SET sz_015_Students_Id = vb_id, sz_015_Using = 1 where sz_015_Licence = pv_Licence;
-    set pi_r = 0;
+    set vv_school_name = (select SZ_006_Name from sz_006_schools join sz_007_school_managers on SZ_006_Id = SZ_007_Schools_Id where SZ_007_Nick like upper(pv_School_Manager_Nick));
+    if ((select sz_015_quantity from sz_015_School_Licences where sz_015_school_id = (SELECT sz_007_schools_id from sz_007_school_managers where SZ_007_Nick like pv_School_Manager_Nick)) > 0) then
+		INSERT INTO `schoolerzz`.`sz_002_students`
+		(`SZ_002_Id`,
+		`SZ_002_Name`,
+		`SZ_002_SN1`,
+		`SZ_002_SN2`,
+		`SZ_002_Birth`,
+		`SZ_002_Nationality`,
+		`SZ_002_Country`,
+		`SZ_002_City`,
+		`SZ_002_PostalCode`,
+		`SZ_002_Address`,
+		`SZ_002_Email`,
+		`SZ_002_Password`
+		)
+		VALUES
+		(UUID_TO_BIN(UUID()),
+		pv_Name,
+		pv_SN1,
+		pv_SN2,
+		pdt_Birth,
+		pv_Nationality,
+		pv_Country,
+		pv_City,
+		pv_PostalCode,
+		pv_Address,
+		pv_Email,
+		md5(pv_Password)
+		);
+		set vb_id = (SELECT SZ_002_Id from sz_002_students where SZ_002_Name like pv_Name and SZ_002_SN1 like pv_SN1 and SZ_002_SN2 like pv_SN2);
+		if pt_Medical not like '' then
+			update sz_002_students set SZ_002_Medical = pt_Medical where SZ_002_Id = vb_id;
+		end if;
+		if pv_SN2 not like '' then
+			update sz_002_students set SZ_002_SN2 = pv_SN2 where SZ_002_Id = vb_id;
+		end if;
+		if pt_Observations not like '' then
+			update sz_002_students set SZ_002_Observations = pt_Observations where SZ_002_Id = vb_id;
+		end if;
+		if pt_Photo_Internal_Route not like '' then
+			update sz_002_students set SZ_002_Photo_Internal_Route = pt_Photo_Internal_Route where SZ_002_Id = vb_id;
+		end if;
+		call CreateNick('S', vv_school_name, vb_id, @r_nick);
+		UPDATE sz_002_students SET SZ_002_Nick = (select @r_nick) WHERE SZ_002_Id = vb_id;
+        UPDATE sz_015_School_Licences SET sz_015_quantity = sz_015_quantity - 1 where sz_015_school_id = (SELECT sz_007_schools_id from sz_007_school_managers where SZ_007_Nick like pv_School_Manager_Nick);
+		set pi_r = 0;
+	else
+		set pi_r = 1;
+	end if;
 end;
 // DELIMITER ;
 
@@ -140,44 +142,49 @@ begin
     declare vv_school_name varchar(50);
     declare vb_id binary(16);
     set vv_school_name = (select SZ_006_Name from sz_006_schools join sz_007_school_managers on SZ_006_Id = SZ_007_Schools_Id where SZ_007_Nick like upper(pv_School_Manager_Nick)); 
-    INSERT INTO `schoolerzz`.`sz_008_teachers`
-    (`SZ_008_Id`,
-    `SZ_008_Name`,
-    `SZ_008_SN1`,
-    `SZ_008_SN2`,
-    `SZ_008_Birth`,
-    `SZ_008_Nationality`,
-    `SZ_008_Country`,
-    `SZ_008_City`,
-    `SZ_008_PostalCode`,
-    `SZ_008_Address`,
-    `SZ_008_Email`,
-    `SZ_008_Password`,
-    `SZ_008_Phone1`
-    )
-    VALUES
-    (UUID_TO_BIN(UUID()),
-    pv_Name,
-    pv_SN1,
-    pv_SN2,
-    pdt_Birth,
-    pv_Nationality,
-    pv_Country,
-    pv_City,
-    pv_PostalCode,
-    pv_Address,
-    pv_Email,
-    pv_Password,
-    pv_Phone1
-    );
-    set vb_id = (SELECT SZ_008_Id from sz_008_teachers where SZ_008_Name like pv_Name and SZ_008_SN1 like pv_SN1 and SZ_008_SN2 like pv_SN2);  
-    if pv_Phone2 not like '' then
-		update sz_008_teachers set sz_008_Phone2 = pv_Phone2 where SZ_008_Id = vb_id;
-    end if;
-    call CreateNick('T', vv_school_name, vb_id, @r_nick);
-    -- set vv_nick = (select @r_nick);
-    UPDATE sz_008_teachers SET SZ_008_Nick = (select @r_nick) WHERE SZ_008_Id = vb_id; 
-    set pi_r = 0;
+	if ((select sz_016_usage from sz_016_t_Licences where sz_016_school_id = (SELECT sz_007_schools_id from sz_007_school_managers where SZ_007_Nick like pv_School_Manager_Nick)) > 0) then
+		INSERT INTO `schoolerzz`.`sz_008_teachers`
+		(`SZ_008_Id`,
+		`SZ_008_Name`,
+		`SZ_008_SN1`,
+		`SZ_008_SN2`,
+		`SZ_008_Birth`,
+		`SZ_008_Nationality`,
+		`SZ_008_Country`,
+		`SZ_008_City`,
+		`SZ_008_PostalCode`,
+		`SZ_008_Address`,
+		`SZ_008_Email`,
+		`SZ_008_Password`,
+		`SZ_008_Phone1`
+		)
+		VALUES
+		(UUID_TO_BIN(UUID()),
+		pv_Name,
+		pv_SN1,
+		pv_SN2,
+		pdt_Birth,
+		pv_Nationality,
+		pv_Country,
+		pv_City,
+		pv_PostalCode,
+		pv_Address,
+		pv_Email,
+		md5(pv_Password),
+		pv_Phone1
+		);
+		set vb_id = (SELECT SZ_008_Id from sz_008_teachers where SZ_008_Name like pv_Name and SZ_008_SN1 like pv_SN1 and SZ_008_SN2 like pv_SN2);  
+		if pv_Phone2 not like '' then
+			update sz_008_teachers set sz_008_Phone2 = pv_Phone2 where SZ_008_Id = vb_id;
+		end if;
+		call CreateNick('T', vv_school_name, vb_id, @r_nick);
+		-- set vv_nick = (select @r_nick);
+		UPDATE sz_008_teachers SET SZ_008_Nick = (select @r_nick) WHERE SZ_008_Id = vb_id; 
+        UPDATE sz_016_t_Licences SET sz_016_usage = sz_016_usage - 1 where sz_016_school_id = (SELECT sz_007_schools_id from sz_007_school_managers where SZ_007_Nick like pv_School_Manager_Nick);
+		set pi_r = 0;
+    else
+		set pi_r = 1;
+	end if;
 end;
 // DELIMITER ;
 
@@ -234,7 +241,7 @@ begin
     pv_PostalCode,
     pv_Address,
     pv_Email,
-    pv_Password,
+    md5(pv_Password),
     pv_Phone1
     );
     set vb_id = (SELECT SZ_003_Id from sz_003_parents where SZ_003_Name like pv_Name and SZ_003_SN1 like pv_SN1 and SZ_003_SN2 like pv_SN2);  
@@ -260,27 +267,20 @@ end;
 
 DROP PROCEDURE IF EXISTS InsertLicenses;
 DELIMITER //
-CREATE PROCEDURE InsertLicenses(in pi_n_lic int)
+CREATE PROCEDURE InsertLicenses(in pi_n_lic int,
+								IN pv_school_manager varchar(15))
 BEGIN
 	declare vb_s_id binary(16);
 	declare vb_t_id binary(16);
-    declare vv_new_s_lic varchar(29);
-    declare vv_new_t_lic varchar(29);
-    declare vi_cont int;
-    
-	set vv_new_s_lic = GenerateLicence();
-	set vb_s_id = (select SZ_015_Id from sz_015_S_licences where SZ_015_Licence like (vv_new_s_lic));
+    declare vv_new_lic varchar(29);
+    DECLARE vi_t_usage INT;
+	set vv_new_lic = GenerateLicence();
+	set vb_s_id = (select SZ_015_Id from sz_015_school_licences where SZ_015_Licence like vv_new_lic);
 	if vb_s_id <=> NULL then
-		insert into sz_015_S_licences(SZ_015_Id, SZ_015_Licence, SZ_015_Usage) values(UUID_TO_BIN(UUID()), vv_new_s_lic,pi_n_lic);
+		insert into sz_015_school_licences(SZ_015_Id, SZ_015_Licence, SZ_015_quantity, sz_015_school_Id) values(UUID_TO_BIN(UUID()), vv_new_lic,pi_n_lic, (select sz_006_Id from sz_006_schools s inner join sz_007_school_managers m on s.sz_006_id = m.SZ_007_Schools_Id where m.SZ_007_Nick = UPPER(pv_school_manager)));
     end if;
-    While pi_n_lic >= 20 Do
-			set vv_new_t_lic = GenerateLicence();
-			set vb_t_id = (select SZ_016_Id from sz_016_t_licences where SZ_016_Licence like (vv_new_t_lic));
-			if vb_t_id <=> NULL then
-				insert into sz_016_t_licences(SZ_016_Id, SZ_016_Licence) values(UUID_TO_BIN(UUID()), vv_new_t_lic);
-			end if;
-        set pi_n_lic = pi_n_lic - 20;
-	END WHILE;
+    set vi_t_usage =truncate((select sz_015_quantity from sz_015_school_licences where sz_015_school_id = (select sz_006_Id from sz_006_schools s inner join sz_007_school_managers m on s.sz_006_id = m.SZ_007_Schools_Id where m.SZ_007_Nick = UPPER(pv_school_manager))) DIV 20,0) + 1;
+	insert into sz_016_t_licences(sz_016_id, SZ_016_Licence, SZ_016_Usage, sz_016_school_Id) values(UUID_TO_BIN(UUID()), GenerateLicence(), vi_t_usage, (select sz_006_Id from sz_006_schools s inner join sz_007_school_managers m on s.sz_006_id = m.SZ_007_Schools_Id where m.SZ_007_Nick = UPPER(pv_school_manager)));
 END
 // DELIMITER ;
 
@@ -301,16 +301,16 @@ BEGIN
     set vv_user = upper(concat(pc_char,pv_username));
     
    IF pc_char = upper('S') then 
-		set vb_id = (Select SZ_002_Id FROM sz_002_students WHERE SZ_002_Nick LIKE vv_user AND SZ_002_Password LIKE pv_password);
+		set vb_id = (Select SZ_002_Id FROM sz_002_students WHERE SZ_002_Nick LIKE vv_user AND SZ_002_Password LIKE md5(pv_password));
 	ELSE
 		IF pc_char = upper('T') then 
-			set vb_id = (Select SZ_008_Id FROM sz_008_teachers WHERE SZ_008_Nick LIKE vv_user AND SZ_008_Password LIKE pv_password);
+			set vb_id = (Select SZ_008_Id FROM sz_008_teachers WHERE SZ_008_Nick LIKE vv_user AND SZ_008_Password LIKE md5(pv_password));
 		ELSE
 			IF pc_char = upper('M') then 
-				set vb_id = (Select SZ_007_Id FROM sz_007_school_managers WHERE SZ_007_Nick LIKE vv_user AND SZ_007_Password LIKE pv_password);
+				set vb_id = (Select SZ_007_Id FROM sz_007_school_managers WHERE SZ_007_Nick LIKE vv_user AND SZ_007_Password LIKE md5(pv_password));
 			ELSE 
 				IF pc_char = upper('P') then 
-					set vb_id = (Select SZ_003_Id FROM sz_003_parents WHERE SZ_003_Nick LIKE vv_user AND SZ_003_Password LIKE pv_password);
+					set vb_id = (Select SZ_003_Id FROM sz_003_parents WHERE SZ_003_Nick LIKE vv_user AND SZ_003_Password LIKE md5(pv_password));
 				END IF;
 			END IF;
 		END IF;
@@ -341,3 +341,29 @@ Begin
 end
 
 // DELIMITER ;
+
+DROP PROCEDURE IF EXISTS LoginTeacherAO;
+DELIMITER //
+CREATE PROCEDURE LoginTeacherAO(IN username VARCHAR(40), IN pwd VARCHAR(250), OUT valid INT)
+BEGIN
+	DECLARE exist BINARY(16);
+    SELECT SZ_008_Id FROM sz_008_teachers  WHERE SZ_008_Nick LIKE username AND SZ_008_Password LIKE pwd INTO exist;
+    IF exist IS NOT NULL THEN 
+		SET valid = 0;
+    ELSE
+		SET valid = -1;
+	END IF;
+END
+// DELIMITER ;
+
+DROP PROCEDURE IF EXISTS GetStudent;
+
+DELIMITER //
+CREATE PROCEDURE GetStudent(in pv_Name varchar(50),
+							in pv_SN1 varchar(100),
+							in pv_SN2 varchar(100))
+BEGIN
+	select * from sz_002_students where sz_002_Name = pv_Name and sz_002_SN1 = pv_SN1 and sz_002_SN2 = pv_SN2; 
+END
+// DELIMITER ;
+
--- BBDD/scriptGeneracion.sql
+++ BBDD/scriptGeneracion.sql
@@ -1,33 +1,37 @@
+USE SchoolerzZ;
+
+DROP TABLE IF EXISTS
+SZ_8014_Teachers_Teachers_Skins,
+SZ_205_Students_Skins,
+SZ_204_Students_Emblems,
+SZ_203_Students_Parents,
+SZ_109_Groups_Activities,
+SZ_102_Groups_Students,
+SZ_003_Parents,
+SZ_015_School_Licences,
+SZ_004_Emblems,
+SZ_005_Skins,
+SZ_017_Subjects,
+SZ_002_Students,
+SZ_007_School_Managers,
+SZ_011_Posts,
+SZ_010_Threads,
+SZ_013_Messages,
+SZ_012_Chats,
+SZ_016_T_Licences,
+SZ_014_Teachers_Skins,
+SZ_009_Activities,
+SZ_001_Groups,
+SZ_008_Teachers,
+SZ_006_Schools;
+
+
+
 DROP DATABASE IF EXISTS schoolerzz;
 
 CREATE DATABASE IF NOT EXISTS SchoolerzZ;
 
 USE SchoolerzZ;
-
-DROP TABLE IF EXISTS
-SZ_001_Groups,
-SZ_002_Students,
-SZ_007_School_Managers,
-SZ_008_Teachers,
-SZ_010_Threads,
-SZ_011_Posts,
-SZ_012_Chats,
-SZ_013_Messages,
-SZ_102_Groups_Students,
-SZ_109_Groups_Activities,
-SZ_203_Students_Parents,
-SZ_204_Students_Emblems,
-SZ_205_Students_Skins,
-SZ_8014_Teachers_Teachers_Skins,
-SZ_003_Parents,
-SZ_004_Emblems,
-SZ_005_Skins,
-SZ_006_Schools,
-SZ_009_Activities,
-SZ_014_Teachers_Skins,
-SZ_015_S_Licences,
-SZ_016_T_Licences,
-SZ_017_Subjects;
 
 CREATE TABLE IF NOT EXISTS SZ_003_Parents(
     SZ_003_Id BINARY(16) PRIMARY KEY,
@@ -58,6 +62,16 @@ CREATE TABLE IF NOT EXISTS SZ_006_Schools(
     SZ_006_Address VARCHAR(200) NOT NULL,
     SZ_006_Deleted TINYINT DEFAULT(0)
 );
+
+CREATE TABLE IF NOT EXISTS SZ_016_T_Licences(
+    SZ_016_Id BINARY(16) PRIMARY KEY,
+    SZ_016_Licence VARCHAR(29)  NOT NULL,
+    SZ_016_Usage INT DEFAULT (0),
+    SZ_016_School_Id BINARY(16),
+    FOREIGN KEY(SZ_016_School_Id)
+        REFERENCES SZ_006_Schools(SZ_006_Id)
+);
+
 CREATE TABLE IF NOT EXISTS SZ_017_Subjects(
 	SZ_017_Id BINARY(16) PRIMARY KEY,
     SZ_017_Name VARCHAR(30) NOT NULL
@@ -72,16 +86,7 @@ CREATE TABLE IF NOT EXISTS SZ_009_Activities(
     SZ_009_Content TEXT NOT NULL,
     SZ_009_Deleted TINYINT DEFAULT(0)
 );
-CREATE TABLE IF NOT EXISTS SZ_015_S_Licences(
-    SZ_015_Id BINARY(16) PRIMARY KEY,
-    SZ_015_Licence VARCHAR(29)  NOT NULL,
-    SZ_015_Usage INT DEFAULT (0)
-);
-CREATE TABLE IF NOT EXISTS SZ_016_T_Licences(
-    SZ_016_Id BINARY(16) PRIMARY KEY,
-    SZ_016_Licence VARCHAR(29)  NOT NULL,
-    SZ_016_Usage INT DEFAULT (0)
-);
+
 CREATE TABLE IF NOT EXISTS SZ_005_Skins(
     SZ_005_Id BINARY(16) PRIMARY KEY,
     SZ_005_Name VARCHAR(25) NOT NULL,
@@ -120,9 +125,7 @@ CREATE TABLE IF NOT EXISTS SZ_002_Students(
     SZ_002_Observations TEXT DEFAULT('Sin observaciones'),
     SZ_002_Photo_Internal_Route TEXT,
     SZ_002_Licence_Id Binary (16),
-    SZ_002_Deleted TINYINT DEFAULT(0),
-    FOREIGN KEY (SZ_002_Licence_Id)
-		REFERENCES sz_015_s_licences(SZ_015_Id)
+    SZ_002_Deleted TINYINT DEFAULT(0)
 );
 CREATE TABLE IF NOT EXISTS SZ_205_Students_Skins(
     SZ_205_Id BINARY(16) PRIMARY KEY,
@@ -188,10 +191,7 @@ CREATE TABLE IF NOT EXISTS SZ_008_Teachers(
     SZ_008_Password VARCHAR(200) NOT NULL,
     SZ_008_Phone1 VARCHAR(15) NOT NULL,
     SZ_008_Phone2 VARCHAR(15),
-    SZ_008_Licence_Id Binary(16),
-    SZ_008_Deleted TINYINT DEFAULT(0),
-    FOREIGN KEY (SZ_008_Licence_Id)
-		REFERENCES sz_016_t_licences(SZ_016_Id)
+    SZ_008_Deleted TINYINT DEFAULT(0)
 );
 CREATE TABLE IF NOT EXISTS SZ_001_Groups(
     SZ_001_Id BINARY(16) PRIMARY KEY,
@@ -290,3 +290,258 @@ CREATE TABLE IF NOT EXISTS SZ_011_Posts(
     FOREIGN KEY(SZ_011_Threads_Id)
         REFERENCES SZ_010_Threads(SZ_010_Id)
 );
+
+CREATE TABLE IF NOT EXISTS SZ_015_School_Licences(
+    SZ_015_Id BINARY(16) PRIMARY KEY,
+    SZ_015_Licence VARCHAR(29)  NOT NULL,
+    SZ_015_Quantity INT DEFAULT(0),
+    SZ_015_School_Id BINARY(16),
+    FOREIGN KEY(SZ_015_School_Id)
+        REFERENCES SZ_006_Schools(SZ_006_Id)
+);
+
+INSERT INTO `schoolerzz`.`sz_006_schools`
+(`SZ_006_Id`,
+`SZ_006_Name`,
+`SZ_006_SchoolCode`,
+`SZ_006_Words3`,
+`SZ_006_Country`,
+`SZ_006_City`,
+`SZ_006_PostalCode`,
+`SZ_006_Address`)
+VALUES
+(UUID_TO_BIN(UUID()),'Joyfe',1,'JOY','Spain', 'Madrid',28017,'c/ Vital Aza, 65');
+
+INSERT INTO `schoolerzz`.`sz_007_school_managers`
+(`SZ_007_Id`,
+`SZ_007_Name`,
+`SZ_007_SN1`,
+`SZ_007_SN2`,
+`SZ_007_Birth`,
+`SZ_007_Nationality`,
+`SZ_007_Country`,
+`SZ_007_City`,
+`SZ_007_PostalCode`,
+`SZ_007_Address`,
+`SZ_007_Email`,
+`SZ_007_Phone1`,
+`SZ_007_Nick`,
+`SZ_007_Password`,
+`SZ_007_Schools_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'María Teresa','Martínez', 'Puche', curdate(),'Spanish','Spain','Madrid','28017','Calle Peter Peter, 4 2Z','mtere@mt.com','+34555555555', 'MJOY-MAMAPU', md5('1234'), (SELECT SZ_006_Id FROM sz_006_schools limit 1));
+
+INSERT INTO `schoolerzz`.`sz_008_teachers`
+(`SZ_008_Id`,
+`SZ_008_Name`,
+`SZ_008_SN1`,
+`SZ_008_SN2`,
+`SZ_008_Birth`,
+`SZ_008_Nationality`,
+`SZ_008_Country`,
+`SZ_008_City`,
+`SZ_008_PostalCode`,
+`SZ_008_Address`,
+`SZ_008_Email`,
+`SZ_008_Password`,
+`SZ_008_Phone1`
+)
+VALUES
+(UUID_TO_BIN(UUID()), 'Guillermo','Pérez', 'Tomé',curdate(),'Spanish','Spain','Madrid','28017','Calle Colegio, 4 2Z','guille@gu.com',md5('1234'), '+34111111111');
+
+INSERT INTO `schoolerzz`.`sz_002_students`
+(`SZ_002_Id`,
+`SZ_002_Name`,
+`SZ_002_SN1`,
+`SZ_002_SN2`,
+`SZ_002_Birth`,
+`SZ_002_Nationality`,
+`SZ_002_Country`,
+`SZ_002_City`,
+`SZ_002_PostalCode`,
+`SZ_002_Address`,
+`SZ_002_Email`,
+`SZ_002_Password`
+)
+VALUES
+(UUID_TO_BIN(UUID()), 'Óscar Vicente','Melgarejo', 'Pino',curdate(),'Spanish','Spain','Madrid','28017','Call Carmencita, 4 2Z','oscar@os.com',md5('1234'));
+
+INSERT INTO `schoolerzz`.`sz_003_parents`
+(`SZ_003_Id`,
+`SZ_003_Name`,
+`SZ_003_SN1`,
+`SZ_003_SN2`,
+`SZ_003_Birth`,
+`SZ_003_Nationality`,
+`SZ_003_Country`,
+`SZ_003_City`,
+`SZ_003_PostalCode`,
+`SZ_003_Address`,
+`SZ_003_Email`,
+`SZ_003_Password`,
+`SZ_003_Phone1`
+)
+VALUES
+(UUID_TO_BIN(UUID()), 'Pater','Noster', 'Muy Noster',curdate(),'Spanish','Spain','Madrid','28017','Calle Yo Claudio, 4 2Z','Pater@pa.com',md5('1234'), '+34666666666');
+
+INSERT INTO `schoolerzz`.`sz_203_students_parents`
+(`SZ_203_Id`,
+`SZ_203_Students_Id`,
+`SZ_203_Parents_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()), (SELECT SZ_002_Id FROM sz_002_students limit 1) , (SELECT SZ_003_Id FROM sz_003_parents limit 1));
+
+INSERT INTO `schoolerzz`.`sz_005_skins`
+(`SZ_005_Id`,
+`SZ_005_Name`,
+`SZ_005_Description`,
+`SZ_005_Route`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Bic Cristal Rojo', 'Skin de Boli Bic Cristal Rojo super molona', 'C:/SchoolerzZ/Skins/bic_rojo.png');
+
+INSERT INTO `schoolerzz`.`sz_205_students_skins`
+(`SZ_205_Id`,
+`SZ_205_Students_Id`,
+`SZ_205_Skins_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),(SELECT SZ_002_Id FROM sz_002_students limit 1), (SELECT SZ_005_Id FROM sz_005_skins limit 1));
+
+INSERT INTO `schoolerzz`.`sz_004_emblems`
+(`SZ_004_Id`,
+`SZ_004_Name`,
+`SZ_004_Description`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Pitagorín', 'Eres el más rápido en cálculo mental');
+
+INSERT INTO `schoolerzz`.`sz_204_students_emblems`
+(`SZ_204_Id`,
+`SZ_204_Students_Id`,
+`SZ_204_Emblems_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),(SELECT SZ_002_Id FROM sz_002_students limit 1), (SELECT SZ_004_Id FROM sz_004_emblems limit 1));
+
+INSERT INTO `schoolerzz`.`sz_001_groups`
+(
+	`SZ_001_Id`,
+    `SZ_001_Stage`,
+    `SZ_001_Level`,
+    `SZ_001_Class`,
+    `SZ_001_Schools_Id`,
+    `SZ_001_Teachers_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Primaria', 1, 'A', (SELECT SZ_006_Id FROM sz_006_schools limit 1), (SELECT SZ_008_Id FROM sz_008_teachers limit 1));
+INSERT INTO SZ_017_Subjects(
+	SZ_017_Id,
+    SZ_017_Name
+)
+ VALUES (UUID_TO_BIN(UUID()), 'Matemáticas');
+INSERT INTO `schoolerzz`.`sz_102_groups_students`
+(
+	`SZ_102_Id`,
+    `SZ_102_Groups_Id`,
+    `SZ_102_Students_Id`,
+     SZ_102_Subjects_Id,
+     SZ_102_Mark_1T,
+     SZ_102_Mark_2T,
+     SZ_102_Mark_3T,
+     SZ_102_Mark_F,
+     SZ_102_Absence_1T,
+     SZ_102_Absence_2T,
+     SZ_102_Absence_3T,
+     SZ_102_Absence_F,
+     SZ_102_J_Absence_1T,
+     SZ_102_J_Absence_2T,
+     SZ_102_J_Absence_3T,
+     SZ_102_J_Absence_F
+)
+VALUES
+(UUID_TO_BIN(UUID()),(SELECT SZ_001_Id FROM sz_001_groups limit 1), (SELECT SZ_002_Id FROM sz_002_students limit 1),(SELECT SZ_017_Id FROM sz_017_subjects limit 1),0,0,0,0,0,0,0,0,0,0,0,0);
+
+INSERT INTO `schoolerzz`.`sz_009_activities`
+(
+    `SZ_009_Id`,
+    `SZ_009_Title`,
+    `SZ_009_Deadline`,
+    `SZ_009_CreationDate`,
+    `SZ_009_UpdateDate`,
+    `SZ_009_Route`,
+    `SZ_009_Content`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Juego Triángulos', '1996-11-22', '1996-11-21', curdate(), 'C:/SchoolerzZ/Activities/tri_game.exe', '-Formas Geométricas -Identificación de Triángulos');
+
+INSERT INTO `schoolerzz`.`sz_109_groups_activities`
+(
+    `SZ_109_Id`,
+    `SZ_109_Groups_Id`,
+    `SZ_109_Activities_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),(SELECT SZ_001_Id FROM sz_001_groups limit 1), (SELECT SZ_009_Id FROM sz_009_activities limit 1));
+
+INSERT INTO `schoolerzz`.`sz_012_chats`
+(
+    `SZ_012_Id`,
+    `SZ_012_Name`,
+    `SZ_012_Image_Route`,
+    `SZ_012_Group_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'1A', 'C:/SchoolerzZ/Chats/chat1A.txt', (SELECT SZ_001_Id FROM sz_001_groups limit 1));
+
+INSERT INTO `schoolerzz`.`sz_013_messages`
+(
+    `SZ_013_Id`,
+    `SZ_013_Body`,
+    `SZ_013_Sent_Date`,
+    `SZ_013_Chats_Id`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Primer Mensaje', curdate(), (SELECT SZ_012_Id FROM sz_012_chats limit 1));
+
+INSERT INTO `schoolerzz`.`sz_014_teachers_skins`
+(`SZ_014_Id`,
+`SZ_014_Name`,
+`SZ_014_Description`,
+`SZ_014_Route`
+)
+VALUES
+(UUID_TO_BIN(UUID()),'Pilot Verde', 'Skin de Pilot Verde de Aprobar', 'C:/SchoolerzZ/Teachers_Skins/pilot_verde.png');
+
+INSERT INTO `schoolerzz`.`sz_8014_teachers_teachers_skins`
+(
+    `SZ_8014_Id`,
+    `SZ_8014_Teachers_Id`,
+    `SZ_8014_Teachers_Skins_Id`
+) 
+VALUES 
+(UUID_TO_BIN(UUID()),(SELECT SZ_008_Id FROM sz_008_teachers limit 1), (SELECT SZ_014_Id FROM sz_014_teachers_skins limit 1));
+
+INSERT INTO `schoolerzz`.`sz_010_Threads`
+(
+	`SZ_010_Id`,
+    `SZ_010_Title`,
+    `SZ_010_CreationDate`,
+    `SZ_010_Teacher_Id`
+)
+VALUES 
+(UUID_TO_BIN(UUID()),'Actividades 1A', curdate(), (SELECT SZ_008_Id FROM sz_008_teachers limit 1));
+
+INSERT INTO `schoolerzz`.`sz_011_posts`
+(
+	`SZ_011_Id`,
+    `SZ_011_Title`,
+    `SZ_011_CreationDate`,
+    `SZ_011_Body`,
+    `SZ_011_Teacher_Id`,
+    `SZ_011_Threads_Id`
+)
+VALUES 
+(UUID_TO_BIN(UUID()),'Primer Post', curdate(), 'Body primer post', (SELECT SZ_008_Id FROM sz_008_teachers limit 1), (SELECT SZ_010_Id FROM sz_010_threads limit 1));
